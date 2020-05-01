a2621b1f142636cdced39b3f54919d4bd3f55903
Ethan <ethanjusino@gmail.com>
2020-04-30 22:48:01 -0500

    Design setup for layouts.

--- app/src/main/java/edu/apsu/weatherapp/LocationSearch.java
+++ app/src/main/java/edu/apsu/weatherapp/LocationSearch.java
@@ -1,7 +1,16 @@
 package edu.apsu.weatherapp;
 
-public class LocationSearch {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LocationSearch extends AppCompatActivity {
     // Class for handling searching for and adding locations to user list
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_search);
+    }
 
 }
