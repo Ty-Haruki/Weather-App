5183ee80c10e8e614215b90b9b185036d334674f
Jones, Tanner <TJONES99@MY.APSU.EDU>
2020-05-04 00:46:01 -0500

    Messed around with widget. (Mainly did research for widgets)

--- app/src/main/java/edu/apsu/weatherapp/WeatherWidgetProvider.java
+++ app/src/main/java/edu/apsu/weatherapp/WeatherWidgetProvider.java
@@ -11,24 +11,32 @@ import java.util.Random;
 
 public class WeatherWidgetProvider extends AppWidgetProvider {
 
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        super.onReceive(context, intent);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int count = appWidgetIds.length;
 
         for (int i = 0; i < count; i++) {
             int widgetId = appWidgetIds[i];
-            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+            String degrees = String.format("%03d", (new Random().nextInt(100))+((char)176));
+            // change degrees to string to a getter method that gets degrees. Same with city
 
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.widget_layout);
-            remoteViews.setTextViewText(R.id.textView, number);
+            remoteViews.setTextViewText(R.id.degreesTV, degrees);
 
             Intent intent = new Intent(context, WeatherWidgetProvider.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
                     0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            //remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
