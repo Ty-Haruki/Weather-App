d30ba5747b69214917b6e598a9b5369834391ccc
Ty-Haruki <ethanjusino@gmail.com>
2020-05-05 16:45:41 -0400

    Added Search for Individual Cities by ID

--- app/src/main/java/edu/apsu/weatherapp/MainActivity.java
+++ app/src/main/java/edu/apsu/weatherapp/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set ID based on Saved City
+        CityInfo cityInfo = new CityInfo(4613868, getApplicationContext());
+        cityInfo.execute();
+
+        // Wait for CityInfo to finish
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("INFO", cityInfo.name + ", " + cityInfo.country);
+        TextView location = findViewById(R.id.location);
+        location.setText(cityInfo.name + ", " + cityInfo.country);
+        TextView current_temp = findViewById(R.id.current_temp);
+        current_temp.setText(cityInfo.temp[0] + "° F");
+        TextView weather_desc = findViewById(R.id.weather_desc);
+        weather_desc.setText(String.valueOf(cityInfo.weather_desc[0]).toUpperCase());
+
         findViewById(R.id.location).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CurrentDay.class);
+                Intent intent = new Intent(getApplicationContext(), LocationDetails.class);
                 startActivity(intent);
             }
         });
